63d1d50195c7c5132bc4020ebffe42711ae9f83f
YuryyBright <79107557+YuryyBright@users.noreply.github.com>
2025-03-19 10:30:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from telethon import TelegramClient, events, sync
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
-api_id = 29139837
-api_hash = '047d9d5420e85de93a6a4bd829f65a99'
+api_id = 12341234
+api_hash = '12341234124'
 
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
